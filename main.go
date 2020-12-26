f711dab88939fc7bb987d83d3370c9455a95b88c
Alex Metelli <alexmetelli.poker@gmail.com>
2020-12-26 16:10:12 +0000

    v0.2

--- main.go
+++ main.go
@@ -20,14 +20,5 @@ func main() {
 
 	words := loadWords()
 	secretWord := chooseWord(words)
-
-	//letters := []string{"o", "k", "l", "d", "w", "n"}
-
-	//loaded := loadWords()
-	//word := chooseWord(loaded)
-	//guessed := getGuessedWord(secretWord, letters)
-	//available := getAvailable(letters)
-	//wordGuessed := wordIsGuessed(secretWord, letters)
-	// fmt.Printf("%t", wordGuessed)
 	hangman(secretWord)
 }
